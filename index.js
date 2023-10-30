492f4d0b49dcb9660d66e6542e55670d99a74556
Mark Bien <48498353+markbien@users.noreply.github.com>
2023-10-30 16:34:03 +0800

    Add constructor for card

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const imagePlaceholder = document.querySelector(".employee-image");
-const employeeName = document.querySelector(".employee-name");
-const employeePosition = document.querySelector(".employee-position");
-const employeeInfo = document.querySelector(".employee-information");
+// const imagePlaceholder = document.querySelector(".employee-image");
+// const employeeName = document.querySelector(".employee-name");
+// const employeePosition = document.querySelector(".employee-position");
+// const employeeInfo = document.querySelector(".employee-information");
+const container = document.querySelector('.container');
 
 function Employee(name, position, info, imagePath) {
   this.name = name;
@@ -22,3 +23,54 @@ const patrick = new Employee(
   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, rerum sapiente praesentium atque dignissimos incidunt consequuntur illum expedita recusandae explicabo dolorem suscipit culpa tempore libero corrupti eligendi sit ut a!",
   "images/patrick.jpeg"
 );
+
+function createCard(employeeName, employeePosition, employeeInformation, imagePath){
+  const imageContainer = document.createElement("div");
+  imageContainer.classList.add("image-container");
+
+  const profilePicture = document.createElement("img");
+  profilePicture.src(imagePath);
+  profilePicture.classList.add("employee-image");
+
+  const background = document.createElement("div");
+  background.classList.add("background");
+
+  imageContainer.appendChild(profilePicture);
+  imageContainer.appendChild(background);
+
+  const employeeName = document.createElement("h4");
+  employeeName.classList.add("employee-name");
+  employeeName.textContent = employeeName;
+
+  const employeePosition = document.createElement("h4");
+  employeePosition.classList.add("employeee-position");
+  employeePosition.textContent = employeePosition;
+
+  const employeeInformation = document.createElement("employee-information");
+  employeeInformation.classList.add("employee-information");
+  employeeInformation.textContent = employeeInformation;
+
+  const pageControls = document.createElement("div");
+  pageControls.classList.add("page-controls");
+
+  const leftButton = document.createElement("img");
+  leftButton.src("images/chevron-left-solid.svg");
+  leftButton.classList.add("arrows");
+
+  const rightButton = document.createElement("img");
+  rightButton.src("images/chevron-right-solid.svg")
+  rightButton.classList.add("arrows");
+
+  pageControls.appendChild(leftButton);
+  pageControls.appendChild(rightButton);
+
+  const surpriseMeButton = document.createElement("button");
+  surpriseMeButton.textContent = "Surprise me!";
+
+  container.appendChild(imageContainer);
+  container.appendChild(employeeName);
+  container.appendChild(employeePosition);
+  container.appendChild(employeeInformation);
+  container.appendChild(pageControls);
+  container.appendChild(surpriseMeButton);
+}
