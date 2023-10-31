f6202458ed14e3be96dd6dcf766768f4c044c091
Mark Bien <48498353+markbien@users.noreply.github.com>
2023-10-31 17:12:01 +0800

    Add test data and set opacity to 0

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-// const imagePlaceholder = document.querySelector(".employee-image");
-// const employeeName = document.querySelector(".employee-name");
-// const employeePosition = document.querySelector(".employee-position");
-// const employeeInfo = document.querySelector(".employee-information");
 const container = document.querySelector('.container');
 
 function Employee(name, position, info, imagePath) {
@@ -11,25 +7,12 @@ function Employee(name, position, info, imagePath) {
   this.imagePath = imagePath;
 }
 
-const spongebob = new Employee(
-  "Spongebob",
-  "Wev Developer",
-  "asdfasdfasdf",
-  "images/spongebob.jpeg"
-);
-const patrick = new Employee(
-  "Patrick",
-  "Designer",
-  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, rerum sapiente praesentium atque dignissimos incidunt consequuntur illum expedita recusandae explicabo dolorem suscipit culpa tempore libero corrupti eligendi sit ut a!",
-  "images/patrick.jpeg"
-);
-
-function createCard(employeeName, employeePosition, employeeInformation, imagePath){
+function createCard(newEmployeeName, newEmployeePosition, newEmployeeInformation, imagePath){
   const imageContainer = document.createElement("div");
   imageContainer.classList.add("image-container");
 
   const profilePicture = document.createElement("img");
-  profilePicture.src(imagePath);
+  profilePicture.src = imagePath;
   profilePicture.classList.add("employee-image");
 
   const background = document.createElement("div");
@@ -40,25 +23,25 @@ function createCard(employeeName, employeePosition, employeeInformation, imagePa
 
   const employeeName = document.createElement("h4");
   employeeName.classList.add("employee-name");
-  employeeName.textContent = employeeName;
+  employeeName.textContent = newEmployeeName;
 
-  const employeePosition = document.createElement("h4");
-  employeePosition.classList.add("employeee-position");
-  employeePosition.textContent = employeePosition;
+  let employeePosition = document.createElement("h4");
+  employeePosition.classList.add("employee-position");
+  employeePosition.textContent = newEmployeePosition;
 
-  const employeeInformation = document.createElement("employee-information");
+  let employeeInformation = document.createElement("employee-information");
   employeeInformation.classList.add("employee-information");
-  employeeInformation.textContent = employeeInformation;
+  employeeInformation.textContent = newEmployeeInformation;
 
   const pageControls = document.createElement("div");
   pageControls.classList.add("page-controls");
 
   const leftButton = document.createElement("img");
-  leftButton.src("images/chevron-left-solid.svg");
+  leftButton.src = "images/chevron-left-solid.svg";
   leftButton.classList.add("arrows");
 
   const rightButton = document.createElement("img");
-  rightButton.src("images/chevron-right-solid.svg")
+  rightButton.src = "images/chevron-right-solid.svg";
   rightButton.classList.add("arrows");
 
   pageControls.appendChild(leftButton);
@@ -67,10 +50,51 @@ function createCard(employeeName, employeePosition, employeeInformation, imagePa
   const surpriseMeButton = document.createElement("button");
   surpriseMeButton.textContent = "Surprise me!";
 
-  container.appendChild(imageContainer);
-  container.appendChild(employeeName);
-  container.appendChild(employeePosition);
-  container.appendChild(employeeInformation);
-  container.appendChild(pageControls);
-  container.appendChild(surpriseMeButton);
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  card.appendChild(imageContainer);
+  card.appendChild(employeeName);
+  card.appendChild(employeePosition);
+  card.appendChild(employeeInformation);
+  card.appendChild(pageControls);
+  card.appendChild(surpriseMeButton);
+
+  return card;
 }
+
+const cardArray = [];
+
+const spongebob = new Employee(
+  "Spongebob",
+  "Wev Developer",
+  "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis incidunt atque ea sunt iusto quae recusandae, asperiores alias nobis laborum dolore, quia aliquid in officiis perferendis et harum, saepe nostrum.",
+  "images/spongebob.jpeg"
+);
+const patrick = new Employee(
+  "Patrick",
+  "Designer",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, rerum sapiente praesentium atque dignissimos incidunt consequuntur illum expedita recusandae explicabo dolorem suscipit culpa tempore libero corrupti eligendi sit ut a!",
+  "images/patrick.jpeg"
+);
+const krab = new Employee(
+  "Mister Krabs",
+  "Lead Programmer",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea autem vitae, eligendi possimus dignissimos nostrum, sit, cum facere id aut maiores odio natus obcaecati nulla! Aspernatur nemo distinctio cum eveniet!",
+  "images/krab.jpeg"
+)
+
+const spongebob_card = createCard(spongebob.name, spongebob.position, spongebob.info, "images/spongebob.jpeg");
+const patrick_card = createCard(patrick.name, patrick.position, patrick.info, "images/patrick.jpeg");
+const krab_card = createCard(krab.name, krab.position, krab.info, "images/krab.jpeg");
+
+cardArray.push(spongebob_card);
+cardArray.push(patrick_card);
+cardArray.push(krab_card);
+
+for (let i = 0; i < cardArray.length; i++) {
+  cardArray[i].setAttribute("id", `card_${i}`);
+  container.appendChild(cardArray[i]);
+}
+
+let currentCardShown = 0;
