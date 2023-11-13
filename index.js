03e6bfc530ab36101851cdf6c0928ba1b3d769ec
Mark Bien <48498353+markbien@users.noreply.github.com>
2023-11-13 18:33:04 +0800

    Declare required variables

---
+++ index.js
@@ -0,0 +1,8 @@
+const image = document.querySelector("#employee-image");
+const name = document.querySelector("#employee-name");
+const position = document.querySelector("#employee-position");
+const info = document.querySelector("#employee-info");
+const leftBtn = document.querySelector("#left-button");
+const rightBtn = document.querySelector("#right-button");
+const randomizeBtn = document.querySelector("#randomize-button");
+
